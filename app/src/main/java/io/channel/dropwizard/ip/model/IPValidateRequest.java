c48136665d3ac06f13d344be2236b011046fd314
Shawn <shawn@channel.io>
2025-09-18 18:40:36 +0900

    @Valid 위치 수정

--- app/src/main/java/io/channel/dropwizard/ip/model/IPValidateRequest.java
+++ app/src/main/java/io/channel/dropwizard/ip/model/IPValidateRequest.java
@@ -1,6 +1,5 @@
 package io.channel.dropwizard.ip.model;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,7 +8,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
-@Valid
 public class IPValidateRequest {
     @Pattern(regexp = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$")
     private String address;
--- app/src/main/java/io/channel/dropwizard/ip/resource/IPResource.java
+++ app/src/main/java/io/channel/dropwizard/ip/resource/IPResource.java
@@ -11,6 +11,7 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import lombok.extern.slf4j.Slf4j;
+import jakarta.validation.Valid;;
 
 @Path("/ip")
 @Slf4j
@@ -26,7 +27,7 @@ public class IPResource {
     @POST
     @Path("/validate")
     @Produces(MediaType.APPLICATION_JSON)
-    public IPValidateView validate(@QueryParam("office") Boolean isOffice, IPValidateRequest body) {
+    public IPValidateView validate(@QueryParam("office") Boolean isOffice, @Valid IPValidateRequest body) {
         log.info("office: {}, address: {}", isOffice, body.getAddress());
         return new IPValidateView(ipValidateBehavior.validate(isOffice, body.getAddress()));
     }
